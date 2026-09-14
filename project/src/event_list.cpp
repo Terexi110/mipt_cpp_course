02ffbdf1bb8b70674045c505e664c4e1ab42d8f4
Albert Brylev <106182827+Terexi110@users.noreply.github.com>
2026-09-14 13:17:50 +0300

    Заготовки для занятия 1.2

---
+++ project/src/event_list.cpp
@@ -0,0 +1,17 @@
+#include <event_list.h>
+
+namespace nano_edr {
+
+void ListPushBack(EventList* list, const Event* event){
+
+}
+
+void ListPopFront(EventList* list){
+
+}
+
+void ListClear(EventList* list){
+    
+}
+
+}
---
+++ project/src/parse.cpp
@@ -0,0 +1,14 @@
+#include <parse.h>
+
+namespace nano_edr {
+
+bool ParseEventLine(const std::string* line, Event* out){
+
+}
+
+
+bool IsBlankOrComment(const std::string* line){
+
+}
+
+}
