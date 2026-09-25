c296c676aa305e4237e5125f1fe538b86c3b8683
Albert Brylev <106182827+Terexi110@users.noreply.github.com>
2026-09-25 20:26:55 +0300

    rules servityname

--- project/src/rules.cpp
+++ project/src/rules.cpp
@@ -1,4 +1,26 @@
 #include "rules.h"
 
-using namespace nano_edr;
+namespace nano_edr {
+
+const char* SeverityName(Severity severity){
+    switch (severity) {
+        case Severity::kLow:
+            return "low";
+        case Severity::kMedium:
+            return "medium";
+        case Severity::kHigh:
+            return "high";
+        case Severity::kCritical:
+            return "critical";
+        default:
+            return "?";
+    }
+}
+
+size_t CheckRules(const Event& event, const Rule* rules, size_t rule_count){
+    return 0;    
+}
+
+
+}
 
